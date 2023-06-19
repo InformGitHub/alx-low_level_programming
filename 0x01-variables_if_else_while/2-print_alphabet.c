0c44a88aa26a531bd88794b348a17e67449e0b11
john(gec) <gec.egc23@gmail.com>
2023-06-19 11:28:29 -0700

    print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	char a[26] = "abcdefghijklmnopqrstuvwxyz";
