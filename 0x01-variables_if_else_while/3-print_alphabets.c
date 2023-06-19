8fa6d0754bdc43d88972b6742fa1519b8763ad69
john(gec) <gec.egc23@gmail.com>
2023-06-19 14:22:23 -0700

    print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the alphabet in lowercase and uppercase
@@ -11,10 +12,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char a[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 53; i++)
+	for (i = 0; i < 52; i++)
 		putchar(a[i]);
 
 	putchar('\n');
