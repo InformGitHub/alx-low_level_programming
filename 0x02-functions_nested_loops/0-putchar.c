d2963b7e0f787c58b1ceaa1f81b07968fda17e5b
john(gec) <gec.egc23@gmail.com>
2023-06-20 07:47:53 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - program that prints _putchar
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
