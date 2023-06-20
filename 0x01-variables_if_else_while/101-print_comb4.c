8e41d78f926c662f208513a5feaaef7fb82be592
john(gec) <gec.egc23@gmail.com>
2023-06-19 18:02:42 -0700

    comb5

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int k, p, u;
+
+	for (k = 48; k <= 58; k++)
+	{
+		for (p = 49; p <= 58; p++)
+		{
+			for (u = 50; u <= 58; u++)
+			{
+				if (u > p && p > k)
+				{
+					putchar(k);
+					putchar(p);
+					putchar(u);
+					if (k != 55 || p != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
