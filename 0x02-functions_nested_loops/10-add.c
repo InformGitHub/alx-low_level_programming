9520a7ee4f6d24f66f87dc618bd588475db54b89
john(gec) <gec.egc23@gmail.com>
2023-06-21 07:48:45 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - checks for lowercase character
+ * @e: value to be added
+ * @f: value to be added
+ *
+ * Description: function
+ * main.h: prototype of function
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int add(int e, int f)
+{
+	int a;
+
+	a = e + f;
+
+	return (a);
+}
