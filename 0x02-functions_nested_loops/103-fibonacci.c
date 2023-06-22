18fc3249afdbddccb7e9b653a8ea77b94a5de841
john(gec) <gec.egc23@gmail.com>
2023-06-21 19:03:55 -0700

    13-fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	unsigned long int sum, follow, b, m;
+
+	b = 1;
+	m = 2;
+	sum = 0;
+
+	for (a = 1; a <= 33; a++)
+	{
+		if (b < 4000000 && (b % 2) == 0)
+		{
+			sum = sum + b;
+		}
+		follow = m + b;
+		b = m;
+		m = follow;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
