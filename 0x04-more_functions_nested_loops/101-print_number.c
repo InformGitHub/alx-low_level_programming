c90db458c60ebc75b74878bd9e7fcb60392aacfe
john(gec) <gec.egc23@gmail.com>
2023-06-22 15:00:20 -0700

    14-numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_number - prints an integer
+ * @n: to be printed
+ *
+ * Description: function
+ * Return:void
+ */
+void print_number(int n)
+{
+	unsigned int h;
+
+	if (n < 0)
+	{
+		h = -n;
+		_putchar('_');
+	}
+	else
+	{
+		h = n;
+	}
+	if (h / 10)
+	{
+		print_number(h / 10);
+	}
+	_putchar((h % 10) + '0');
+}
