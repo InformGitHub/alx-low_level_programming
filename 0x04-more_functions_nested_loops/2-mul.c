53c5455d9081a76c938222a7d94415c43e6a371d
john(gec) <gec.egc23@gmail.com>
2023-06-22 06:30:27 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: digit to be multiplied
+ * @b: digit to be multiplied
+ *
+ * Description: function
+ * Return:a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
