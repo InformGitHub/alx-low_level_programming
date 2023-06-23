602d164b40a7cf7112358ac5dcfd5cb3e8550407
john(gec) <gec.egc23@gmail.com>
2023-06-22 20:24:26 -0700

    pos_neg

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,9 +11,9 @@
 void positive_or_negative(int i)
 {
 	if (i > 0)
-		printf("i is positive\n");
+		printf("%d is positive\n", i);
 	else if (i == 0)
-		printf("i is zero\n");
+		printf("%d is zero\n", i);
 	else
-		printf("i is negative\n");
+		printf("%d is negative\n", i);
 }
