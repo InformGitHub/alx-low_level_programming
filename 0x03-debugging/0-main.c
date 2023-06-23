b0b97fd1fbe3989aeb941dd6861c2c0dfd41bf7a
john(gec) <gec.egc23@gmail.com>
2023-06-22 18:59:14 -0700

    main01

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - test for positive or negative
+ *
+ * Description: function
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
