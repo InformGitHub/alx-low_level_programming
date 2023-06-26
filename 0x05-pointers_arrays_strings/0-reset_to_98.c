db0d2452e14a7a45fab0de26a23b0d9ef89b0f8d
john(gec) <gec.egc23@gmail.com>
2023-06-26 05:01:54 -0700

    R98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * reset_to_98 - checks for uppercase character.
+ * @*n: pointer to be checked
+ *
+ * Description: function
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
