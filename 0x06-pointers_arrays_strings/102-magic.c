c99c603eddc09345fbc69ed62e1d732d76d8b6f2
john(gec) <gec.egc23@gmail.com>
2023-06-29 19:46:26 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	int n;
+	int m;
 	int a[5];
-	int *p;
+	int *k;
 
 	a[2] = 1024;
-	p = &n;
-	*(p + 5) = 98;
+	k = &m;
+	*(k + 5) = 98;
 	printf ("a[2] = %d\n", a[2]);
 	return (0);
 
