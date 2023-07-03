1e1c8a79431e67bce3ac8c3c07837ba020721046
john(gec) <gec.egc23@gmail.com>
2023-07-03 08:36:59 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @c: desired value
+ *
+ * @s:starting address of memory
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int k = 0;
+
+	for (; s[k] >= '\0'; k++)
+	{
+		if (s[k] == c)
+		{
+			return (&s[k]);
+		}
+	}
+	return (0);
+}
