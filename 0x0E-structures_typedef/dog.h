59e343f7497b4bb538263d082600e8318f31a27e
john(gec) <gec.egc23@gmail.com>
2023-07-17 12:56:44 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's structure
+ * @name: name's initializing
+ * @age: age's initializing
+ * @owner: owner's initializing
+ *
+ * Description: struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
