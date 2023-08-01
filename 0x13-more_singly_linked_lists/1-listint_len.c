ed96bb9a6f49c4dfae81d0a62a26f6504d155884
john(gec) <gec.egc23@gmail.com>
2023-08-01 06:38:46 -0700

    listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * print_listint - prints a linked list
+  * @h: pointer
+  *
+  * return: size
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		h = h->next;
+		k++;
+	}
+	return (k);
+}
